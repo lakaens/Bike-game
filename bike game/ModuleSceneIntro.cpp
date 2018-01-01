b4038c5f4a578c550fd22df35b03557e9fd297c4
lakaens <lakaens12@gmail.com>
2018-01-01 20:53:45 +0100

    update map 1

--- bike game/ModuleSceneIntro.cpp
+++ bike game/ModuleSceneIntro.cpp
@@ -21,14 +21,19 @@ bool ModuleSceneIntro::Start()
 	App->camera->Move(vec3(1.0f, 1.0f, 0.0f));
 	App->camera->LookAt(vec3(0, 0, 0));
 	cube1.size = vec3(4,0.05,20);
-	cube2.size = vec3(4,0.05, 5);
-	cube1.color=White;
-	cube2.color = White;
+	cube1.color = White;
 	cube1.SetPos(0, 0, 10);
-	App->physics->AddBody(cube1, 1000);
-	cube2.SetPos(0, 0, 10);
-	App->physics->AddBody(cube2, 1000);
+	cube_1=App->physics->AddBody(cube1, 2000);
+	cube2.size = vec3(5,0.05, 4);
+	cube2.color = White;
+	cube2.SetPos(-4.5, 0, 18);
+	cube_2=App->physics->AddBody(cube2, 2000);
+	cube3.size=vec3(5,2,4);
+	cube3.color = White;
+	cube3.SetPos(-9,0,18);
+	cube3.SetRotation(23.f, vec3(0,0,-1));
 	
+	cube_3=App->physics->AddBody(cube3, 2000);
 	
 
 	return ret;
@@ -48,7 +53,11 @@ update_status ModuleSceneIntro::Update(float dt)
 	Plane p(0, 1, 0, 0);
 	p.axis = true;
 	cube1.Render();
-	//cube2.Render();
+	cube_1->GetTransform(&cube1.transform);
+	cube2.Render();
+	cube_2->GetTransform(&cube2.transform);
+	cube3.Render();
+	cube_3->GetTransform(&cube3.transform);
 	p.Render();
 
 	return UPDATE_CONTINUE;
--- bike game/ModuleSceneIntro.h
+++ bike game/ModuleSceneIntro.h
@@ -42,6 +42,6 @@ public:
 	PhysMotor3D* left_wheel;
 	PhysMotor3D* right_wheel;
 
-	Cube cube1;
-	Cube cube2;
+	Cube cube1, cube2,cube3;
+	PhysBody3D *cube_1,*cube_2,*cube_3;
 };
