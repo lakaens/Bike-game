a10d1f4f6c28144ede9d5007e020c268d89659c7
lakaens <lakaens12@gmail.com>
2018-01-01 18:58:17 +0100

    Bike Game

---
+++ bike game/ModulePlayer.h
@@ -0,0 +1,33 @@
+#pragma once
+#include "Module.h"
+#include "Globals.h"
+#include "p2Point.h"
+
+struct PhysVehicle3D;
+
+#define MAX_ACCELERATION 1500.0f
+#define TURN_DEGREES 20.0f * DEGTORAD
+#define BRAKE_POWER 75.0f
+
+class ModulePlayer : public Module
+{
+public:
+	ModulePlayer(Application* app, bool start_enabled = true);
+	virtual ~ModulePlayer();
+
+	bool Start();
+	update_status Update(float dt);
+	bool CleanUp();
+
+public:
+
+	PhysVehicle3D* vehicle;
+	float turn;
+	float acceleration;
+	float brake;
+
+private:
+	uint currentTime = 0;
+	uint TimeFlag = 0;
+	float brakedelay = 0;
+};
---
+++ bike game/ModuleSceneIntro.cpp
@@ -0,0 +1,60 @@
+#include "Globals.h"
+#include "Application.h"
+#include "ModuleSceneIntro.h"
+#include "Primitive.h"
+#include "PhysBody3D.h"
+#include"PhysVehicle3D.h"
+
+ModuleSceneIntro::ModuleSceneIntro(Application* app, bool start_enabled) : Module(app, start_enabled)
+{
+}
+
+ModuleSceneIntro::~ModuleSceneIntro()
+{}
+
+// Load assets
+bool ModuleSceneIntro::Start()
+{
+	LOG("Loading Intro assets");
+	bool ret = true;
+
+	App->camera->Move(vec3(1.0f, 1.0f, 0.0f));
+	App->camera->LookAt(vec3(0, 0, 0));
+	cube1.size = vec3(4,0.05,20);
+	cube2.size = vec3(4,0.05, 5);
+	cube1.color=White;
+	cube2.color = White;
+	cube1.SetPos(0, 0, 10);
+	App->physics->AddBody(cube1, 1000);
+	cube2.SetPos(0, 0, 10);
+	App->physics->AddBody(cube2, 1000);
+	
+	
+
+	return ret;
+}
+
+// Load assets
+bool ModuleSceneIntro::CleanUp()
+{
+	LOG("Unloading Intro scene");
+
+	return true;
+}
+
+// Update
+update_status ModuleSceneIntro::Update(float dt)
+{
+	Plane p(0, 1, 0, 0);
+	p.axis = true;
+	cube1.Render();
+	//cube2.Render();
+	p.Render();
+
+	return UPDATE_CONTINUE;
+}
+
+void ModuleSceneIntro::OnCollision(PhysBody3D* body1, PhysBody3D* body2)
+{
+}
+
